ffe7f5ae0bf1a4de2dbd94f1cca45d50c31d8372
thrashraf <zulashrafvns@gmail.com>
2021-06-18 09:09:13 +0800

    add es6 features

--- app.js
+++ app.js
@@ -1,51 +1,24 @@
+// get target
+const input = document.querySelector('[name = display]');
+const button = document.querySelectorAll('.button');
 
-//i target input display and put in in a variable
-const display = document.discal.display;
+//fucntion
+const inputDisplay = (event) => {
+    const buttonClicked = event.target.value;
+   
+    if(buttonClicked == '='){
+        display.value = eval(display.value);
+    }else if(buttonClicked == 'del'){
+        display.value = display.value.slice(0 , -1);
+    }else if (buttonClicked == ' '){
+        display.value = ' ';
+    }else{
+        display.value += buttonClicked;
+    } 
+};
 
-//this function is how to display number on display input
-function get_val (value){
-
-     display.value+=value;
-}
-// this functon is how to clearscreen on display calculator using C on calculator button
-function cls(){
-
-    display.value = " " 
-}
-// this function is how to calculate 
-function calculate(){
-
-    display.value = eval(display.value);
-    
-}
-//this function is how to delete a number on display
-function Delete(){
-
-    var Delete = display.value.slice(0 , -1);
-    display.value = Delete;
-}
-
-//this function is how to type number and the number appear on display & also after we complete the number that we want , 
-// hit enter and t will calculate 
-// i want to make when we hit backspace , the number will delete but soon or later i will think
-document.addEventListener('keypress' , function(event){
-
-    
-    
-
-    if (event.key === 'Enter') {
-
-        
-        
-        event.preventDefault();
-        calculate();
-          
-    }
-    else {
-
-            document.discal.display.value += event.key;
-     
-    }
+//addEventListner;
+button.forEach((buttons) => {
+   buttons.addEventListener('click' , inputDisplay);
 });
-
 
